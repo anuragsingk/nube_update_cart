a3bd8ebf50f3d188393703c3163c732d72abf1c2
Anurag Singh <83174475+anuragsingk@users.noreply.github.com>
2024-09-28 13:38:20 +0530

    Add files via upload
    First release for WordPress 6.5.4 and WooCommerce 9.3.3.

---
+++ nube_update_cart.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name: NubeCart Update
+Description: Updates cart quantity dynamically using JavaScript, eliminating the need to click the 'Update Cart' button..
+Version: 1.0
+Author: Nubevest Pvt Ltd
+Plugin URI: https://nubevest.com.au
+*/
+
+defined('ABSPATH') || die('No script kiddies please!');  // Prevent direct access
+
+/* Init for Translation ready */
+function nubeUpdate_init() {
+    load_plugin_textdomain('nube_update_cart', false, dirname(plugin_basename(__FILE__)) . '/languages');
+}
+add_action('init', 'nubeUpdate_init');
+
+/* Update Cart using JavaScript */
+add_action('wp_footer', 'nube_cartUpdate');
+function nube_cartUpdate() {
+    if (is_cart()) :
+    ?>
+ <script>
+        jQuery(function($) {
+            var timeout;
+
+            // Trigger cart update when quantity changes
+            $('div.woocommerce').on('input change', '.qty', function() {
+                clearTimeout(timeout);
+
+                timeout = setTimeout(function() {
+                    // Submit the cart form automatically
+                    $('button[name="update_cart"]').trigger('click');
+                }, 500); // Delay to prevent immediate submission
+            });
+        });
+    </script>
+
+    <?php
+    endif;
+}
+
+/* CSS to Hide the "Update Cart" Button */
+function buttonHide() {
+    echo '
+    <style type="text/css">
+        button[name="update_cart"] {
+            display: none !important;
+        }
+    </style>';
+}
+add_action('wp_print_styles', 'buttonHide');
